1e04b61ab093dcc782ab3d226ed8071ed0fea492
Behnam Pordavam <behnampordavam@gmail.com>
2024-03-13 08:56:06 +0000

    adding has_size to db

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -1,4 +1,4 @@
-ALLOWED_HOSTS = ['8000-bennigithub-boutiqueado-z2l4ft22w9x.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-bennigithub-boutiqueado-z2l4ft22w9x.ws-eu109.gitpod.io']
 """
 Django settings for boutique_ado project.
 
