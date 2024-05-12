313534aac7595463c7ac9e58ee081b86a3baeeb1
Behnam Pordavam <behnampordavam@gmail.com>
2024-05-12 12:29:46 +0000

    modify verification  email settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -116,7 +116,7 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 
-ACCOUNT_EMAIL_VERIFICATION = 'none'
+# ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
